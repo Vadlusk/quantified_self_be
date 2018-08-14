c0d836aa43454d612bff0feb440587c439e181b8
VadLusk <AdLusk@gmail.com>
2018-08-14 13:01:27 -0600

    Add simplecov setup.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,10 @@
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __dir__)
 abort('The Rails env is running in production mode!') if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start 'rails'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
