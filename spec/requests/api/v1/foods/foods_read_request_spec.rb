d8f78efafca472841310792e5629ccc32edf92ac
VadLusk <AdLusk@gmail.com>
2018-08-14 16:19:53 -0600

    Refactor json_response out of test and into support.

--- spec/requests/api/v1/foods/foods_read_request_spec.rb
+++ spec/requests/api/v1/foods/foods_read_request_spec.rb
@@ -4,14 +4,12 @@ describe 'foods API' do
   context 'sends' do
     it 'all foods' do
       create_list(:food, 20)
-      
+
       get '/api/v1/foods'
 
-      json = JSON.parse(response.body, symbolize_names: true)
-
       expect(response.status).to eq(200)
-      expect(json.length).to eq(20)
-      json.each do |food|
+      expect(json_response.length).to eq(20)
+      json_response.each do |food|
         expect(food.keys).to contain_exactly(
           :id,
           :name,
