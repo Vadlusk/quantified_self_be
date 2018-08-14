2f55739e6f2576aaa9e230f441c2b32d7ace321f
VadLusk <AdLusk@gmail.com>
2018-08-14 17:13:28 -0600

    Add happy and sad path tests for food show.

--- spec/requests/api/v1/foods/foods_read_request_spec.rb
+++ spec/requests/api/v1/foods/foods_read_request_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe 'foods API' do
+  let(:food) { create(:food) }
+
   context 'sends' do
     it 'all foods' do
       create_list(:food, 20)
@@ -15,6 +17,25 @@ describe 'foods API' do
           :name,
           :calories
         )
+      end
+    end
+    it 'one specific food' do
+      get "/api/v1/foods/#{food.id}"
+
+      expect(response.status).to eq(200)
+      expect(json_response.keys).to contain_exactly(
+        :id,
+        :name,
+        :calories
+      )
+    end
+  end
+  context 'does not send' do
+    context 'one specific food when' do
+      it 'the food does not exist' do
+        get "/api/v1/foods/#{food.id}"
+
+        expect(response.status).to eq(404)
       end
     end
   end
