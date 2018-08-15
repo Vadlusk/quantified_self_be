2775ce4b847774cb9467858218e5ef0837f29e91
VadLusk <AdLusk@gmail.com>
2018-08-15 16:07:12 -0600

    Add meal food create test.

---
+++ spec/requests/api/v1/meals/foods/meal_foods_create_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'meal foods API' do
+  let(:meal) { create(:meal) }
+  let(:food) { create(:food, meal: meal) }
+  let(:message) { "Successfully added #{food.name} to #{meal.name}" }
+  
+  context 'creates a meal food' do
+    it 'with correct params' do
+      post "/api/v1/meals/#{meal.id}/foods/#{food.id}"
+
+      expect(response.status).to eq(201)
+      expect(json_response[:message]).to eq(message)
+    end
+  end
+  context 'does not create a meal food' do
+    it 'if the food does not exist' do
+      post "/api/v1/meals/#{meal.id}/foods/#{food.id - 1}"
+
+      expect(response.status).to eq(404)
+    end
+    it 'if the meal does not exist' do
+      post "/api/v1/meals/#{meal.id - 1}/foods/#{food.id}"
+
+      expect(response.status).to eq(404)
+    end
+  end
+end
