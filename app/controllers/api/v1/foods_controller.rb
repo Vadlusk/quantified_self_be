123312a5dfa8842534b5c6de5d72288752d9e164
VadLusk <AdLusk@gmail.com>
2018-08-14 18:35:51 -0600

    Add sad path.

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -13,6 +13,8 @@ class Api::V1::FoodsController < ApplicationController
     food = Food.new(food_params)
     food.save!
     render json: food, status: 201
+  rescue ActiveRecord::RecordInvalid
+    render status: 400
   end
 
   private
@@ -25,5 +27,6 @@ class Api::V1::FoodsController < ApplicationController
 
     def food_params
       params.require(:food).permit(:name, :calories)
+    rescue ActionController::ParameterMissing
     end
 end
