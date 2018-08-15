4da55483bfc5239ff93ade253d667de8716ea6cb
VadLusk <AdLusk@gmail.com>
2018-08-14 19:49:07 -0600

    Add skeleton for food edit test.

---
+++ spec/requests/api/v1/foods/foods_edit_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'foods API' do
+  context 'edits a food' do
+    it 'with only a name' do
+
+    end
+    it 'with only calories' do
+
+    end
+    it 'with name and calories' do
+
+    end
+  end
+  context 'does not edit a food' do
+    it 'without params' do
+
+    end
+    it 'with params with nil values' do
+
+    end
+    it 'if the food does not exist' do
+      
+    end
+  end
+end
