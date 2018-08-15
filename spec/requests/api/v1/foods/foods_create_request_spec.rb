5365b45135920d9cf32b428434580c22a512089c
VadLusk <AdLusk@gmail.com>
2018-08-14 18:09:05 -0600

    Add skeleton for test.

---
+++ spec/requests/api/v1/foods/foods_create_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'foods API' do
+  context 'creates a food' do
+    it 'with correct params' do
+    end
+  end
+  context 'does not create a food' do
+    it 'if name is missing' do
+
+    end
+    it 'if calories is missing' do
+
+    end
+  end
+end
