a02571115d08d299e3de4486f1bed89f4b3ad0ed
VadLusk <AdLusk@gmail.com>
2018-08-15 17:14:45 -0600

    Add meal show tests.

--- spec/requests/api/v1/meals/meals_read_request_spec.rb
+++ spec/requests/api/v1/meals/meals_read_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'meals API' do
+  let(:meal) { create(:meal_with_foods) }
   context 'sends' do
     it 'all meals' do
       create_list(:meal, 20)
@@ -16,6 +17,32 @@ describe 'meals API' do
           :foods
         )
         expect(meal[:foods]).to eq([])
+      end
+    end
+    it 'a specific meal' do
+      get "/api/v1/meals/#{meal.id}/foods"
+
+      expect(response.status).to eq(200)
+      expect(json_response.keys).to contain_exactly(
+        :id,
+        :name,
+        :foods
+      )
+      json_response[:foods].each do |food|
+        expect(food.keys).to contain_exactly(
+          :id,
+          :name,
+          :calories
+        )
+      end
+    end
+  end
+  context 'does not send' do
+    context 'a specific meal' do
+      it 'if the meal does not exist' do
+        get "/api/v1/meals/#{meal.id - 1}/foods"
+
+        expect(response.status).to eq(404)
       end
     end
   end
