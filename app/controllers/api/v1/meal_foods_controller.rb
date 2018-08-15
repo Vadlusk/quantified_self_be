5e56a7a8e9deff058e53646167dd7069273fd23c
VadLusk <AdLusk@gmail.com>
2018-08-15 16:54:13 -0600

    Add content to create method.

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::MealFoodsController < ApplicationController
   def create
+    meal         = Meal.find(params[:meal_id])
+    food         = Food.find(params[:id])
+    food.meal_id = meal.id
+    message      = "Successfully added #{food.name} to #{meal.name}"
+    render json: { message: message }, status: 201
   end
 end
