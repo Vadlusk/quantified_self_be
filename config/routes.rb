f12fcce07f1801f452c16fd1a5a42787c4abb07d
VadLusk <AdLusk@gmail.com>
2018-08-15 15:33:07 -0600

    Add meals resources in routes and meals controller with index method.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, except: %i[new edit]
+      resources :meals, only: %i[index]
     end
   end
 end
--- spec/requests/api/v1/meals/meals_read_request_spec.rb
+++ spec/requests/api/v1/meals/meals_read_request_spec.rb
@@ -3,18 +3,20 @@ require 'rails_helper'
 describe 'meals API' do
   context 'sends' do
     it 'all meals' do
-      list = create_list(:meal, 20)
+      create_list(:meal, 20)
 
       get '/api/v1/meals'
 
       expect(response.status).to eq(200)
       expect(json_response.length).to eq(20)
-      expect(json_response.keys).to contain_exactly(
-        :id,
-        :name,
-        :foods
-      )
-      expect(json_response[:foods]).to eq([])
+      json_response.each do |meal|
+        expect(meal.keys).to contain_exactly(
+          :id,
+          :name,
+          :foods
+        )
+        expect(json_response[:foods]).to eq([])
+      end
     end
   end
 end
