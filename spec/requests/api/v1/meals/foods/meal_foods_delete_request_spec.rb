f77f9d62eb3f995e3601b80864d933eead39a62c
VadLusk <AdLusk@gmail.com>
2018-08-16 02:48:07 -0600

    Add meal food delete test.

---
+++ spec/requests/api/v1/meals/foods/meal_foods_delete_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'meal foods API' do
+  let(:meal) { create(:meal_with_foods) }
+  let(:food) { meal.foods.first }
+  let(:message) { "Successfully removed #{food.name} from #{meal.name}" }
+  
+  context 'deletes a meal food' do
+    it 'with correct params' do
+      expect(meal.foods.length).to eq(5)
+
+      delete "/api/v1/meals/#{meal.id}/foods/#{meal.foods.first.id}"
+
+      expect(meal.foods.length).to eq(4)
+      expect(response.status).to eq(200)
+      expect(json_response[:message]).to eq(message)
+    end
+  end
+end
