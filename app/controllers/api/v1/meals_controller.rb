3af2dd9c8d99b32a2eef5d2f3f75585092a7d179
VadLusk <AdLusk@gmail.com>
2018-08-15 20:43:53 -0600

    Add route, show method, and set_meal.

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::MealsController < ApplicationController
+  before_action :set_meal, only: %i[show]
+
   def index
     render json: Meal.all
   end
+
+  def show
+    render json: @meal
+  end
+
+  private
+
+    def set_meal
+      @meal = Meal.find(params[:meal_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, except: %i[new edit]
-      resources :meals, only: %i[index show] do
-        resources :foods, only: %i[index]
+      resources :meals, only: %i[index] do
+        get  '/foods',       to: 'meals#show'
         post '/foods(/:id)', to: 'meal_foods#create'
       end
     end
