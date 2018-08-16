29d12d5c23b12371676472e60ecc16e4d8a916f7
VadLusk <AdLusk@gmail.com>
2018-08-15 21:08:28 -0600

    Refactor meal foods controller.

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::MealFoodsController < ApplicationController
+  before_action :set_food, :set_meal
+
   def create
-    meal         = Meal.find(params[:meal_id])
-    food         = Food.find(params[:id])
-    food.meal_id = meal.id
-    message      = "Successfully added #{food.name} to #{meal.name}"
+    @food.meal_id = @meal
+    message = "Successfully added #{@food.name} to #{@meal.name}"
     render json: { message: message }, status: 201
-  rescue ActiveRecord::RecordNotFound
-    render status: 404
   end
+
+  private
+
+    def set_food
+      @food = Food.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render status: 404
+    end
+
+    def set_meal
+      @meal = Meal.find(params[:meal_id])
+    rescue ActiveRecord::RecordNotFound
+      render status: 404
+    end
 end
